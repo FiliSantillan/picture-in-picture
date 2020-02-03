4f85f1723eed60df949eecb4cd0fb6b4bc9070fb
Fili Santillán <fili06604@gmail.com>
2020-02-03 06:32:02 -0600

    :sparkles: Demo complete

--- pip.js
+++ pip.js
@@ -2,13 +2,28 @@ const video = document.getElementById("video"),
       button = document.getElementById("button");
 
 button.addEventListener("click", async () => {
+  button.disabled = true;
 
-  if(document.pictureInPictureElement === video) {
-    await document.exitPictureInPicture();
-    button.innerText = "Activar PIP";
-  } else {
-    await video.requestPictureInPicture();
-    button.innerText = "Desactivar PIP";
+  try {
+    if (document.pictureInPictureElement === video) {
+      await document.exitPictureInPicture();
+      button.innerText = "Activar PIP";
+    } else {
+      await video.requestPictureInPicture();
+      button.innerText = "Desactivar PIP";
+    }
+  } catch (error) {
+    console.log(`Ocurrión un error: ${error}`);
+  } finally {
+    // Habilitamos el botón después del evento.
+    button.disabled = false;
   }
-
 });
+
+video.addEventListener("enterpictureinpicture", function(event) { 
+  console.log(event);
+ });
+
+ video.addEventListener("leavepictureinpicture", function(event) {
+   console.log(event);
+ });
